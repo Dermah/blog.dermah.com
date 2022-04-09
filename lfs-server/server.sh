47540fa1ee7ef66ca8b0af089eb786c8c40823c9
Sputnik <inquiry@dermah.com>
2022-04-09 18:43:39 +1000

    Track a branch of lfs server that will keep up to date

--- lfs-server/server.sh
+++ lfs-server/server.sh
@@ -3,4 +3,4 @@
 echo Authenticated as $(aws sts get-caller-identity --query Arn --output text)
 echo Starting server...
 
-npx "github:Dermah/node-git-lfs#4b79bee4"
+npx "github:Dermah/node-git-lfs#fix-batch-schema"
